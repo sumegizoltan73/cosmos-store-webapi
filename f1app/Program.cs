92f993e82c8dd16012d3a8806f9a9981606a9b2d
Zoltán Péter Sümegi <57563804+sumegizoltan73@users.noreply.github.com>
2024-11-14 02:23:46 +0100

    javítás

--- f1app/Program.cs
+++ f1app/Program.cs
@@ -13,10 +13,10 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddScoped<IDriverRepository, DriverRepository>(x =>
     new DriverRepository(
-        builder.Configuration.GetConnectionString("CosmosDb",
+        builder.Configuration.GetConnectionString("CosmosDb"),
         builder.Configuration["CosmosConfig:primaryKey"],
         builder.Configuration["CosmosConfig:databaseName"],
-        builder.Configuration["CosmosConfig:containerName"])
+        builder.Configuration["CosmosConfig:containerName"]
     ));
 
 var app = builder.Build();
--- f1app/obj/Debug/net9.0/f1app.AssemblyInfo.cs
+++ f1app/obj/Debug/net9.0/f1app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("F1App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae20babbaed5c134e85e9a88ec0ebb8b16bf589c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f354dbc75d6f3c4d1b316d8c494965257a01137")]
 [assembly: System.Reflection.AssemblyProductAttribute("F1App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("F1App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
